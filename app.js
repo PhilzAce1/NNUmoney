ac9903b3995b3d4893278b043fbfd6bc28876468
Philzace1 <unknownumeh@gmail.com>
2019-11-07 16:21:44 +0100

    first

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-import {login} from './lib/login';
-import cron from 'node-cron'
+require('dotenv').config()
+import {login, getBal} from './lib/login';
+// import cron from 'node-cron'
+// import express from 'express';
+// const app =express()
 
-// import {sharePost} from './lib/sharePost';
+// import {sharePos t} from './lib/sharePost';
 import db from './lib/db'
-
+import { sharePost } from './lib/sharePost';
 const go = async () => {
    try {
    const users = db.get('user').value()
@@ -11,18 +14,71 @@ const go = async () => {
       await login(`${user.username}`, `${user.password}`)
   
     }
-//     // await sharePost()
-//     process.exit()
    } catch (error) {
     console.error('Login Failed \n\n', error)   
-    process.exit(0)
+    process.exit(1)
 
    }
 }
+const goShare= async () => {
+  try {
+  const users = db.get('user').value()
+  for (let user of users) {  
+      await sharePost(`${user.username}`, `${user.password}`,`${user.fbu}`,`${user.fbp}`)
+    }
+  } catch (error) {
+      console.error('Login Failed \n\n', error)   
+      process.exit(0)
+    }
+}
 
+// app.get("/",async (req, res) => {
+//   console.log('scrapping')
+// try {
+//   // res.send('working');
+//    await go()
+//    res.redirect('/done');
+// } catch (error) {
+//   res.status(400).json({
+//     message:error
+//   }).send('did not work oooo ... ');
+// }
+// });
+
+// app.post("/user", async(req, res) => {
+//   const username = req.body.username
+//   const password = req.body.password
+//   const addUser = async(username, password) => {
+//     return (
+//       db.get('user')
+//     .push({ username, password})
+//     .write()  
+//     )
+//   }
+//   try {
+//     await addUser(username, password)
+
+//   } catch (error) {
+//     res.send("User created ");
+
+//   }
+// });
+// app.get("/lazy", async(req, res) => {
+//   res.send('You are lazy ooo ... instead of you to do it one by one ... lol') 
+//   await go()
+
+// });
+
+// app.get("/getbalance", async (req, res) => {
+//   let bal = await getBalit()
+//   res.send(bal);
+// });
+
+// app.get("/done", (req, res) => {
+//   res.send('working ooo ')
+// });
+// const port = process.env.PORT || 5000;
+
+// app.listen(port, () => console.log(`Server running on port ${port} 🔥`));
+ 
 go()
- 
-var task = cron.schedule('* * */5 * * *', () =>  {
-  go()
-});
- 
--- play.js
+++ play.js
@@ -1,7 +1,22 @@
-const getUserAgent = () => {
-    const headers = [{'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}, {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'}, {'User-Agent': 'Opera/9.80 (Macintosh; Intel Mac OS X 10.14.1) Presto/2.12.388 Version/12.16'}, {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.75.14 (KHTML, like Gecko) Version/7.0.3 Safari/7046A194A'}, {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14931'}, {'User-Agent': 'Mozilla/5.0 (Windows NT 6.2; WOW64; rv:63.0) Gecko/20100101 Firefox/63.0'}, {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:10.0) Gecko/20100101 Firefox/62.0'}, {'User-Agent': 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.13; ko; rv:1.9.1b2) Gecko/20081201 Firefox/60.0'}, {'User-Agent': 'Chrome (AppleWebKit/537.1; Chrome50.0; Windows NT 6.3) AppleWebKit/537.36 (KHTML like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393'}, {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; AS; rv:11.0) like Gecko'}]
-    let x =Math.round(Math.random() * (10 - 0) )
-  return  (JSON.stringify(headers[x]).replace(/{|}|'User-Agent'/gi, ""))
-  }
-  
-  console.log(getUserAgent())
+// import {sharePost} from './lib/sharePost';
+
+// const go = async() => {
+//   try {
+//     await sharePost()
+//   } catch (error) {
+//     console.log(error)
+//   }
+// }
+
+// go()
+
+// console.log((isPalindrome = (word) => (((word.split('').reverse().join('').toLowerCase()) === (word.toLowerCase())))
+// )('wow'))
+
+// const dummy = () => {let feeback ="Naza is a girl" ; return feeback}
+// console.log(dummy())
+const genericFunction = () => {
+  let languages = ['Python', 'JavaScript', 'Java', 'C#', 'C++']; //declare an array of elements
+  let [firstLang, secondLang, ...otherLanguages] = languages; //otherLanguages will be equal to ['Java', 'C#', 'C++'] 
+  return `First language is ${firstLang} and the second is ${secondLang}. The rest are ${otherLanguages}`}
+console.log(genericFunction());
