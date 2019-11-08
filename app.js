d25465c20640b0981b0db8c5f07ad945a24d9cf0
Philzace1 <unknownumeh@gmail.com>
2019-11-08 13:13:15 +0100

    Finally

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 require('dotenv').config()
 import {login, getBal} from './lib/login';
-import cron from 'node-cron'
-import express from 'express';
-const app =express()
+
 
 // import {sharePos t} from './lib/sharePost';
 import db from './lib/db'
 import { sharePost } from './lib/sharePost';
 const go = async () => {
-   try {
+   try{
    const users = db.get('user').value()
   for (let user of users) {  
       await login(`${user.username}`, `${user.password}`)
@@ -20,6 +18,7 @@ const go = async () => {
 
    }
 }
+
 const goShare= async () => {
   try {
   const users = db.get('user').value()
@@ -32,20 +31,8 @@ const goShare= async () => {
     }
 }
 
-app.get("/",async (req, res) => {
-  console.log('scrapping')
-try {
-  // res.send('working');
-   await go()
-  //  res.redirect('/done');
-  res.send('something')
-} catch (error) {
-  res.status(400).json({
-    message:error
-  }).send('did not work oooo ... ');
-}
-});
 
+go()
 // app.post("/user", async(req, res) => {
 //   const username = req.body.username
 //   const password = req.body.password
@@ -64,22 +51,3 @@ try {
 
 //   }
 // });
-// app.get("/lazy", async(req, res) => {
-//   res.send('You are lazy ooo ... instead of you to do it one by one ... lol') 
-//   await go()
-
-// });
-
-// app.get("/getbalance", async (req, res) => {
-//   let bal = await getBalit()
-//   res.send(bal);
-// });
-
-app.get("/done", (req, res) => {
-  res.send('working ooo ')
-});
-const port = process.env.PORT || 5000;
-
-app.listen(port, () => console.log(`Server running on port ${port} 🔥`));
- 
-// go()
